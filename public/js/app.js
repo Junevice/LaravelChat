6c46aa87e0f6b52c40798d6b687ef6a2afae8ec6
Glenn Oberlé <oberle.glenn@icloud.com>
2022-02-07 17:58:03 +0100

    Fin séance 07/02/2022

--- public/js/app.js
+++ public/js/app.js
@@ -2093,10 +2093,13 @@ window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/d
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
   key: "anyKey",
-  forceTLS: true,
+  forceTLS: false,
   wsHost: window.location.hostname,
   wsPort: 6001,
   disableStats: true
+});
+window.Echo.channel('DemoChannel').listen('WebsocketDemoEvent', function (e) {
+  console.log(e);
 });
 
 /***/ }),
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -23,8 +23,14 @@ window.Pusher = require('pusher-js');
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
-    forceTLS: true,
+    forceTLS: false,
     wsHost: window.location.hostname,
     wsPort: 6001,
     disableStats: true,
 });
+
+window.Echo.channel('DemoChannel')
+.listen('WebsocketDemoEvent', (e) => {
+    console.log(e)
+})
+
